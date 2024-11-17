76caec074786ee6cfea0db57fc9e6f76e6678662
Preethikabala K <132378796+preethika-11@users.noreply.github.com>
2024-11-17 18:13:07 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+import streamlit as st
+
+st.title("BotBuddy")
+input_txt=st.text_input("Please enter your queries here...")
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [("system","you are a helpful AI assistant.Your name is BotBuddy"),
+     ("user","user query:{query}")
+    ])
+llm=Ollama(model="llama2-7b")
+output_parser=StrOutputParser()
+chain = prompt|llm|output_parser
+
+if input_txt:
+    st.write(chain.invoke({"query":input_txt}))
+    
